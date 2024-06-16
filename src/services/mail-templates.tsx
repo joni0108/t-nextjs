c9ad304433dea3f18054794285cd38f45f176348
joni0108 <trollpro0108@icloud.com>
2024-06-15 20:07:12 -0700

    added mailing template

--- src/services/mail-templates.tsx
+++ src/services/mail-templates.tsx
@@ -1,16 +1,18 @@
-import * as React from 'react'
+import * as React from "react";
 
 interface EmailTemplateProps {
-	name: string
+	name: string;
 }
 
-const DemoEmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({ name }) => {
+const DemoEmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
+	name,
+}) => {
 	return (
 		<div>
 			<h1>Hello {name}!</h1>
 			<p>This is a demo email template.</p>
 		</div>
-	)
-}
+	);
+};
 
-export { DemoEmailTemplate }
+export { DemoEmailTemplate };
